dc9a309af916c211d7cd45a511f94ccdde05b7c8
Kendall Roden <kendall@diagrid.io>
2023-05-09 13:07:29 -0500

    fix

--- CheckoutService/Models.cs
+++ CheckoutService/Models.cs
@@ -4,9 +4,9 @@ namespace CheckoutServiceWorkflowSample.Models
     public record InventoryItem(int ProductId, string Name, double PerItemCost, int Quantity);
     public record InventoryResult(bool Available, InventoryItem? productItem, double TotalCost);
 
-    public record CustomerOrder(bool failCheckout, string Name, string Email, OrderItem OrderItem);
+    public record CustomerOrder(bool FailCheckout, string Name, string Email, OrderItem OrderItem);
 
-    public record PaymentRequest(bool failCheckout, string RequestId, string Name, string Email, double TotalCost);
+    public record PaymentRequest(bool FailCheckout, string RequestId, string Name, string Email, double TotalCost);
     public record PaymentResponse(bool Success); 
 
     public record CheckoutResult(bool Processed);
