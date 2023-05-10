fb15851d080e918a3bb6982672a78b9a7d1c4b84
Kendall Roden <kendall@diagrid.io>
2023-05-10 11:49:24 -0700

    update types of products

--- RetailApp/CheckoutService/Controllers/InventoryController.cs
+++ RetailApp/CheckoutService/Controllers/InventoryController.cs
@@ -38,9 +38,9 @@ public class InventoryController : ControllerBase
     {
         var baseInventory = new List<InventoryItem>
         {
-            new InventoryItem(ProductId: 0, Name: itemKeys[0], PerItemCost: 5, Quantity: 100),
-            new InventoryItem(ProductId: 1, Name: itemKeys[1], PerItemCost: 15000, Quantity: 100),
-            new InventoryItem(ProductId: 2, Name: itemKeys[2], PerItemCost: 500, Quantity: 100),
+            new InventoryItem(ProductId: 0, Name: itemKeys[0], PerItemCost: 500, Quantity: 100),
+            new InventoryItem(ProductId: 1, Name: itemKeys[1], PerItemCost: 1000, Quantity: 100),
+            new InventoryItem(ProductId: 2, Name: itemKeys[2], PerItemCost: 10, Quantity: 100),
         };
 
         foreach (var item in baseInventory)
